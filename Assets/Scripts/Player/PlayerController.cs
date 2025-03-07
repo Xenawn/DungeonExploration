6e4f775d16514bce1d2cc6e826264606173bdba8
Xenawn <codeparadoxist@gmail.com>
2025-03-07 12:52:59 +0900

    [Update] 점프 구현

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,16 +8,17 @@ public class PlayerController : MonoBehaviour
     [Header("Movement")]
     public float moveSpeed;
     private Vector2 curMovementInput; // 현재 입력 값
-    public float jumPower;
+
+    public float jumpPower;
     public LayerMask groundLayerMask;
 
 
     private Rigidbody rigidbody;
- 
+    private Animator animatior;
     void Awake()
     {
-
         rigidbody = GetComponent<Rigidbody>();
+        animatior = GetComponent<Animator>();
     }
 
     void Start()
@@ -34,6 +35,10 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
+        if (IsGrounded())
+        {
+            animatior.SetBool("Jumping", false);
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -41,18 +46,53 @@ public class PlayerController : MonoBehaviour
         if(context.phase == InputActionPhase.Performed)
         {
             curMovementInput = context.ReadValue<Vector2>();
+           animatior.SetBool("Moving", true);
         }
         else if(context.phase == InputActionPhase.Canceled)
         {
             curMovementInput = Vector2.zero;
+           animatior.SetBool("Moving", false);
         }
     }
     private void Move()
     {
         Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
         dir *= moveSpeed;
-        dir.y = rigidbody.velocity.y;
-        rigidbody.velocity = dir; // 연산된 속도를 velocity에 넣어준다.
+        dir.y = rigidbody.velocity.y;  // 기존 y 속도를 유지
+        rigidbody.velocity = dir;
 
+
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && IsGrounded())
+        {
+            rigidbody.AddForce(Vector2.up * jumpPower, ForceMode.Impulse);
+            animatior.SetBool("Jumping", true);
+        }
+        
+    }
+
+    bool IsGrounded()
+    {
+        Ray[] rays = new Ray[4]
+        {
+            new Ray(transform.position + (transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.forward * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (transform.right * 0.2f) + (transform.up * 0.01f), Vector3.down),
+            new Ray(transform.position + (-transform.right * 0.2f) +(transform.up * 0.01f), Vector3.down)
+        };
+
+        // 4개의 Ray 중 groundLayerMask에 해당하는 오브젝트가 충돌했는지 조회한다.
+        for (int i = 0; i < rays.Length; i++)
+        {
+            if (Physics.Raycast(rays[i], 0.2f, groundLayerMask))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
